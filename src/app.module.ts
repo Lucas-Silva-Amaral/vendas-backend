86f60ba22f979763828d714a15f085c194f50d51
lucasamaral <lukinhasilvaamaral@gmail.com>
2023-09-12 17:44:57 -0300

    :art: Feat: add password hashed, getAllUsers and Create User

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
-import { Module } from '@nestjs/common';
-import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
+import { Module } from '@nestjs/common'
+import { AppService } from './app.service'
+import { UserModule } from './user/user.module'
 
 @Module({
   imports: [UserModule],
   controllers: [],
-  providers: [AppService],
+  providers: [AppService]
 })
 export class AppModule {}
